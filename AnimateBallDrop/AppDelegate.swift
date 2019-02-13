e548625797c4341feae91b4264c85b805e4fc296
JARBerry <rayberry@rays-macbook.local>
2019-02-13 11:17:31 +0000

    Updated comments

--- AnimateBallDrop/AppDelegate.swift
+++ AnimateBallDrop/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  AnimateBallDrop
 //
-//  Created by Ray Berry on 15/10/2018.
+//  Created by James and Ray Berry on 15/10/2018.
 //  Copyright © 2018 JARBerry. All rights reserved.
 //
 
--- AnimateBallDrop/ViewController.swift
+++ AnimateBallDrop/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  AnimateBallDrop
 //
-//  Created by Ray Berry on 15/10/2018.
+//  Created by James and Ray Berry on 15/10/2018.
 //  Copyright © 2018 JARBerry. All rights reserved.
 //
 
